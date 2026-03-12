118206daf0808f71bbcaecb43f6e78ae783f5baa
val4git <val4git@gmail.com>
2026-03-12 18:33:46 +0300

    add tasks from 2nd lesson

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,6 +4,10 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
-
+export const omit = (main_obj, ...fields) => {
+ let obj = main_obj; 
+    for (let i = 0; i < fields.length; i++) {
+        delete obj[fields[i]];
+};
+return obj;
 };
